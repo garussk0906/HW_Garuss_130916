8da120e7660ec24074d82e55d2b7a4b138d69ff6
Garuss Ekaterina <garussk0906@gmail.com>
2024-04-17 20:11:54 +0500

    1 задача 7 лр

---
+++ src/lr7/Main.java
@@ -0,0 +1,23 @@
+package lr7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SuperClass superClassObject =
+                new SuperClass("передал в конструктор суперкласса");
+        String className = superClassObject.toString();
+        System.out.println(className);
+
+        SubClassTest subClassObject1 =
+                new SubClassTest("передал в конструктор подкласса");
+        String subClassName = subClassObject1.toString();
+        System.out.println(subClassName);
+
+        SubClassTest subClassObject2 =
+                new SubClassTest("передал в конструктор подкласса", "где два параметра");
+        String subClassName2 = subClassObject2.toString();
+        System.out.println(subClassName2);
+    }
+
+}
