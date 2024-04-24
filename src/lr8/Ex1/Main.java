404a75450c6374ffd50dcb84eeeab4ce27b30d52
Garuss Ekaterina <garussk0906@gmail.com>
2024-04-24 16:28:13 +0500

    1-4 пример 8лр

---
+++ src/lr8/Ex1/Main.java
@@ -0,0 +1,26 @@
+package lr8.Ex1;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File f1 = new File("MyFile1.txt");
+            f1.createNewFile();
+            if(f1.exists()) {
+                System.out.println("Создан");
+                System.out.println("Полный путь1: " + f1.getAbsolutePath());
+            }
+
+            File f2 = new File("C:\\Users\\garus\\Рабочий стол\\prog\\MyFile2.txt");
+            f2.createNewFile();
+            System.out.println("Полный путь2: " + f2.getAbsolutePath());
+
+            File f3 = new File("C:\\Papka1\\Papka2\\Papka3");
+            f3.mkdirs();
+            System.out.println("Полный путь3 : " + f3.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println("Ошибка" + e);
+        }
+    }
+}
---
+++ src/lr8/Ex3/Main.java
@@ -0,0 +1,44 @@
+package lr8.Ex3;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Arrays;
+
+public class Main {
+    public static void readAllByArray (InputStream in) throws IOException {
+        byte[] buff = new byte[5];
+        while (true) {
+            int count = in.read(buff);
+            if(count != -1) {
+                System.out.println("Количество = " + count + ", buff = " +
+                        Arrays.toString(buff) + ", str = " +
+                        new String(buff, 0,count,"cp1251"));//"UTF8"
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "C:\\Users\\garus\\Рабочий стол\\Новая папка\\Klipka.txt";
+        InputStream inFile = null;
+
+        try {
+            inFile = new FileInputStream(fileName);
+            readAllByArray(inFile);
+        } catch (IOException e) {
+            System.out.println("Ошибка открытия - закрытия файлв " + fileName + e);
+        } finally {
+            if (inFile != null) {
+                try {
+                    inFile.close();
+                } catch (IOException ignore) {
+                    /*NOP*/
+                }
+            }
+        }
+    }}
+
